13eb85201daaa2ac989521cb6f42e44bb26f6200
Onwuagbaizu Stephanie <127240541+steph2502@users.noreply.github.com>
2024-12-20 13:38:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const connectDB = require('./db');
 const telegramRoute = require('./routes/telegram');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
