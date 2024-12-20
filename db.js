4972ef6de2fcc3d65f1a06e15fa13b7d1aa83086
Onwuagbaizu Stephanie <127240541+steph2502@users.noreply.github.com>
2024-12-20 20:02:15 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const dbURI = process.env.DATABASE_URL || 'mongodb+srv://sonwuagbaizu2202300:EEe8UjbCfXel3u8i@cluster0.efvro.mongodb.net/finderdb?retryWrites=true&w=majority&appName=Cluster0';
-        await mongoose.connect(dbURI);
+        await mongoose.connect(dbURI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
